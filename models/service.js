be212b88c459ecca646ada500a0ea629448744ab
Wesley Shih <shih.wesley@gmail.com>
2019-08-01 11:59:29 -0700

    init & setup mongoDB schema and seeds for service

---
+++ models/service.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const serviceSchema = new Schema({
+  name: { type: String, required: true },
+  plan: { type: String, required: true },
+  usersAmount: { type: Double, required: true },
+  price: {type: Double, required: true},
+  description: {type: String},
+});
+
+const Service = mongoose.model("Service", serviceSchema);
+
+module.exports = Service;
